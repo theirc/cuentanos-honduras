661054e9cb7041a669e666029ed8bb74b46721c0
Bianca Chami <b.chami39@gmail.com>
2023-09-13 13:10:12 +0100

    feat: fixing error

--- lib/menu.ts
+++ lib/menu.ts
@@ -35,8 +35,7 @@ export function getFooterItems(
 // TODO Update menu items if needed.
 export function getMenuItems(
   strings: CustomMenuOverlayStrings,
-  categories: ZendeskCategory[] | CategoryWithSections[],
-  includeAbout: boolean
+  categories: ZendeskCategory[] | CategoryWithSections[]
 ): MenuOverlayItem[] {
   let items: MenuOverlayItem[] = [];
   items.push({ key: 'home', label: strings.home, href: '/' });
@@ -44,13 +43,6 @@ export function getMenuItems(
     addMenuItemsCategories(items, categories as CategoryWithSections[]);
   } else {
     addMenuItemsInformation(items, strings, categories as ZendeskCategory[]);
-  }
-  if (includeAbout) {
-    items.push({
-      key: 'about',
-      label: strings.about,
-      href: `/articles/${ABOUT_US_ARTICLE_ID}`,
-    });
   }
   return items;
 }
